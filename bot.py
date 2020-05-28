30c5b99002e7b537fd5753a65300dc5ed0c16cad
Akhil Pullela <akpullela@gmail.com>
2020-05-28 13:49:32 -0700

    figured out asyncio tasks on test branch

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 import datetime
 import os
 import asyncio
+from periodic import Periodic
 
 
 class MyClient(discord.Client):
@@ -14,16 +15,13 @@ class MyClient(discord.Client):
 
 
 
+
     async def on_message(self, message):
-        if message.content == 'activate':
-            channel = self.get_channel(707642477997195356)
-            #week1
-            pics = ['https://cdn.discordapp.com/attachments/713149370421477490/713150085151719514/190105_miraclenight324_2.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150081800470538/190105_miraclenight324_1.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150099928121414/190105_37minago_2.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150073533366322/1570161842-egan1fvwkaed8cv.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150023214301264/171030__LIKEY___2.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150018676326420/2019-03-24_28.jpg', 'https://cdn.discordapp.com/attachments/713149370421477490/713150016386105434/4theTWICE-1082972550534094848-20190109_200911-img3.jpg']
-            i = random.randint(0, len(pics)-1)
-            if pics[i] in pics:
-                await channel.send(pics[i])
-                pics.remove(pics[i])
-            await asyncio.sleep(86400)
+        if message.content == 'test':
+            while message.content != 'stop':
+                await message.channel.send('`periodic`')
+                await asyncio.sleep(1)
+
 
 
 
