81493198a570403b5966a0a7574b80a18d625e05
Akhil Pullela <akpullela@gmail.com>
2020-06-06 23:21:21 -0700

    added daily-word

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import random
 import datetime
 import os
 import asyncio
+from bs4 import BeautifulSoup
+from googletrans import Translator
+import requests
 
 
 
@@ -16,28 +19,23 @@ class MyClient(discord.Client):
             channel = client.get_channel
             await message.channel.purge(limit=150)
             await message.channel.send('this channel has been cleansed')
+        
+        
+
+        
         if message.content == 'daily-mina':
-            days = 18
+            days = 27
             i = 0
             channel = client.get_channel(713149370421477490)
             pics = [
-                'https://cdn.discordapp.com/attachments/138194552837111808/715316841370878093/18f93d0d731738a77d9d00a6276110bd.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715317021906042890/19592b6928c9774b0ba32ff3833eade2.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715317267356975144/a09ed40d742442ef6083133428769c65.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715317529337266357/41f11a46e7ed2e117bfe480b0296bdce.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715317341755277335/0f9791b7521bc060f83e9f7f4c6ab4fc.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715500100557865031/unknown.png',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715438453822586920/hotwice-751444273703165952-20160708_235410-img4.jpg',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715415435612258405/CzVT7pLUAAAcieq.jpg',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715762678370402394/EZJ1nxFVAAAKNEU.png'
-                'https://pbs.twimg.com/media/EZJ2CvSU0AAleIj.jpg:orig'
-                'https://cdn.discordapp.com/attachments/138194552837111808/715505094338019369/TTV5_Mina25.jpg'
-                'https://images-ext-1.discordapp.net/external/FG7Wwd0FgpGWhAfplwORBAWcuTY5p1E9dA_S40MvIvo/https/pbs.twimg.com/media/CzyYkQ7UsAAMhfF.jpg%3Alarge?width=1016&height=677'
-                'https://media.discordapp.net/attachments/138194552837111808/715317316186931250/05093fc8406dd0b48e31a5e4313b98bf.png?width=508&height=677'
-                'https://images-ext-1.discordapp.net/external/3RTnAteR8-SR9VxHWTs-_1pInFbzrOKcw3xq28cozF8/https/pbs.twimg.com/media/EYcFjcWUMAUeUWo.jpg%3Aorig?width=451&height=677'
-                'https://pbs.twimg.com/media/EYY7OJEU0AAyjfF.jpg:orig'
-                'https://pbs.twimg.com/media/EYY7OJLVcAAqT4D.jpg:orig'
-                'https://gfycat.com/fancyimaginarybuzzard']
+                'https://pbs.twimg.com/media/EZJ2CvSU0AAleIj.jpg:orig',
+                'https://cdn.discordapp.com/attachments/138194552837111808/715505094338019369/TTV5_Mina25.jpg',
+                'https://images-ext-1.discordapp.net/external/FG7Wwd0FgpGWhAfplwORBAWcuTY5p1E9dA_S40MvIvo/https/pbs.twimg.com/media/CzyYkQ7UsAAMhfF.jpg%3Alarge?width=1016&height=677',
+                'https://media.discordapp.net/attachments/138194552837111808/715317316186931250/05093fc8406dd0b48e31a5e4313b98bf.png?width=508&height=677',
+                'https://images-ext-1.discordapp.net/external/3RTnAteR8-SR9VxHWTs-_1pInFbzrOKcw3xq28cozF8/https/pbs.twimg.com/media/EYcFjcWUMAUeUWo.jpg%3Aorig?width=451&height=677',
+                'https://pbs.twimg.com/media/EYY7OJEU0AAyjfF.jpg:orig',
+                'https://pbs.twimg.com/media/EYY7OJLVcAAqT4D.jpg:orig',
+                'https://gfycat.com/fancyimaginarybuzzard',]
             while True:
                 await message.channel.send('```Day ' + str(days) + '```')
                 days += 1
@@ -50,8 +48,24 @@ class MyClient(discord.Client):
                     await message.channel.send('akhil, you pabo, we are on day 35 you need to add more images!')
                     break
                 await asyncio.sleep(86400)
+        if message.content == 'word':
+            url = 'https://www.koreanclass101.com/korean-phrases/'
+            channel = client.get_channel(719070843287896117)
+            while True:
+                source = requests.get(url).text
+                soup = BeautifulSoup(source, 'lxml')
+                word = soup.find('div', class_= 'r101-wotd-widget__word' ).text
+                translator = Translator()
+                translation1 = translator.translate(word, dest = 'te').text 
+                translation2 = translator.translate(word, dest = 'en').text
+                await message.channel.send('KOREAN WORD OF THE DAY:')
+                await message.channel.send('            ' + word + '            ')
+                await message.channel.send('English TRANSLATION: '+translation2)
+                await message.channel.send('Telugu TRANSLATION: ' +translation1)    
+                await asyncio.sleep(86400)        
 
 
+        
 
 
 client = MyClient()
