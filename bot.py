a5eb3e1f10a58fecf22477dd820fa43c200296bd
Akhil Pullela <akpullela@gmail.com>
2020-06-19 12:32:27 -0700

    no changes

--- bot.py
+++ bot.py
@@ -24,18 +24,18 @@ class MyClient(discord.Client):
 
         
         if message.content == 'daily-mina':
-            days = 27
+            days = 34
             i = 0
             channel = client.get_channel(713149370421477490)
             pics = [
-                'https://pbs.twimg.com/media/EZJ2CvSU0AAleIj.jpg:orig',
-                'https://cdn.discordapp.com/attachments/138194552837111808/715505094338019369/TTV5_Mina25.jpg',
-                'https://images-ext-1.discordapp.net/external/FG7Wwd0FgpGWhAfplwORBAWcuTY5p1E9dA_S40MvIvo/https/pbs.twimg.com/media/CzyYkQ7UsAAMhfF.jpg%3Alarge?width=1016&height=677',
-                'https://media.discordapp.net/attachments/138194552837111808/715317316186931250/05093fc8406dd0b48e31a5e4313b98bf.png?width=508&height=677',
-                'https://images-ext-1.discordapp.net/external/3RTnAteR8-SR9VxHWTs-_1pInFbzrOKcw3xq28cozF8/https/pbs.twimg.com/media/EYcFjcWUMAUeUWo.jpg%3Aorig?width=451&height=677',
-                'https://pbs.twimg.com/media/EYY7OJEU0AAyjfF.jpg:orig',
-                'https://pbs.twimg.com/media/EYY7OJLVcAAqT4D.jpg:orig',
-                'https://gfycat.com/fancyimaginarybuzzard',]
+                'https://cdn.discordapp.com/attachments/138194552837111808/721477905263362170/unknown.png',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721520040142176316/DooDoo_22-838105251429277696-20170305_031357-img1.jpg',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721520250453229638/DooDoo_22-838265101132021764-20170305_134908-img1.jpg',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721477402378764399/tumblr_ojtu340bTY1uacoejo1_1280.png',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721477447199096852/C-FD8meVoAM9DNd.png',
+                'https://pbs.twimg.com/media/EaWBcCFUMAAwrJe.jpg:large',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721380342837018694/478427e99bb7f14316f23ac957509a66.png',
+                'https://cdn.discordapp.com/attachments/138194552837111808/721309352895709215/e99a2a9e50ff6e0ec58a6c55d9b5d25f.png',]
             while True:
                 await message.channel.send('```Day ' + str(days) + '```')
                 days += 1
@@ -44,8 +44,8 @@ class MyClient(discord.Client):
                 await message.channel.send(selected_pic)
                 i += 1
 
-                if days == 34:
-                    await message.channel.send('akhil, you pabo, we are on day 35 you need to add more images!')
+                if days == 41:
+                    await message.channel.send('akhil, you pabo, we are on day 40 you need to add more images!')
                     break
                 await asyncio.sleep(86400)
         if message.content == 'word':
