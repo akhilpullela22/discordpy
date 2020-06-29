3a0e6759820ecc3fe9d0f5864cf9ed849e2972c7
Akhil Pullela <akpullela@gmail.com>
2020-06-29 11:39:00 -0700

    added commas to the list (dumb error)

--- Cogs/daily_pic.py
+++ Cogs/daily_pic.py
@@ -19,13 +19,13 @@ class daily_pic(commands.Cog):
             i = 0
             
             pics = [
-                'https://cdn.discordapp.com/attachments/138194552837111808/726895341026279524/wnaxecfrdc131.png'
-                'https://cdn.discordapp.com/attachments/138194552837111808/726934631940751401/dreamnara0324-972965310419496960-20180312_063941-img1.jpg'
-                'https://cdn.discordapp.com/attachments/138194552837111808/727026524527525938/EKpkPdVUYAA7rCo.png'
-                'https://cdn.discordapp.com/attachments/138194552837111808/726824222193614929/TWICELIGHTS_in_JAPAN_Red_Postcards_SCANS_3.jpeg'
-                'https://cdn.discordapp.com/attachments/138194552837111808/726758490856489060/20200627_143101.jpg'
-                'https://cdn.discordapp.com/attachments/138194552837111808/726590783527977040/llw7tukqwgn11.jpg'
-                'https://images-ext-1.discordapp.net/external/R08yiMfa3Pe3BQdxWgdtRDNVjt0BIXTfy_5NqswWXh8/https/pbs.twimg.com/media/EazcK-WU8AAJYSj.jpg%3Alarge?width=845&height=475'
+                'https://cdn.discordapp.com/attachments/138194552837111808/726895341026279524/wnaxecfrdc131.png',
+                'https://cdn.discordapp.com/attachments/138194552837111808/726934631940751401/dreamnara0324-972965310419496960-20180312_063941-img1.jpg',
+                'https://cdn.discordapp.com/attachments/138194552837111808/727026524527525938/EKpkPdVUYAA7rCo.png',
+                'https://cdn.discordapp.com/attachments/138194552837111808/726824222193614929/TWICELIGHTS_in_JAPAN_Red_Postcards_SCANS_3.jpeg',
+                'https://cdn.discordapp.com/attachments/138194552837111808/726758490856489060/20200627_143101.jpg',
+                'https://cdn.discordapp.com/attachments/138194552837111808/726590783527977040/llw7tukqwgn11.jpg',
+                'https://images-ext-1.discordapp.net/external/R08yiMfa3Pe3BQdxWgdtRDNVjt0BIXTfy_5NqswWXh8/https/pbs.twimg.com/media/EazcK-WU8AAJYSj.jpg%3Alarge?width=845&height=475',
                 'https://cdn.discordapp.com/attachments/138194552837111808/726115878730727445/tumblr_p1gvkrW1xc1uacoejo1_1280.png',
                 'https://cdn.discordapp.com/attachments/138194552837111808/725837934078590986/f59850b6ed8210fb8b31730b9c8056cd.jpg',
                 'https://cdn.discordapp.com/attachments/138194552837111808/725824432051978301/image0.png',
