bcbd6a62a8413471563cea4357f6bd8085fe781d
Akhil Pullela <44563625+akhilpullela22@users.noreply.github.com>
2025-01-17 18:46:28 -0700

    Update tba_cog.py
    Hid auth-key for security reasons

--- Cogs/tba_cog.py
+++ Cogs/tba_cog.py
@@ -5,7 +5,7 @@ paragraphs = 1
 formats = 'text'
 response = requests.get("https://www.thebluealliance.com/api/v3/status",
  headers={
-   "X-TBA-Auth-Key": "wvbc47ZZiHo67u4tVA5oHDE7jeNzoWg8kiM3YP4R7PFuAl0M0GUfGoSfVVdjNb3g"
+   "X-TBA-Auth-Key": "auth-key"
  }
 )
 print(response.text)
