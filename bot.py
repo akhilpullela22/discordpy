09876b8ca22793007664b62a5fd26bceb982721f
Akhil Pullela <44563625+akhilpullela22@users.noreply.github.com>
2025-06-13 14:40:31 -0400

    Working tba implementation with team names, schools and location

--- bot.py
+++ bot.py
@@ -3,29 +3,46 @@ import random
 import datetime
 import os
 import asyncio
-from bs4 import BeautifulSoup
-from googletrans import Translator
+#from bs4 import BeautifulSoup
+#from googletrans import Translator
 import requests
 from discord.ext import commands
 
-Token = 'token'
+with open("C:\\Users\\akhil\\Documents\\Bot_keys\\bot_token.txt", "r") as file:
+    Token = file.read()
+with open("C:\\Users\\akhil\\Documents\\Bot_keys\\tba_auth.txt", "r") as file:
+    auth_key = file.read()
 
-bot = commands.Bot(command_prefix = '.')
-bot.remove_command('help')
-         
-@bot.event
-async def on_ready():
-    print('Status: Online') 
-    activity = discord.Activity(name = 'TWICE', type = discord.ActivityType.listening)
-    return await bot.change_presence(activity = activity)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
 
-for filename in os.listdir('./Cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'Cogs.{filename[:-3]}')
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
 
-        
-      
+        if '!team' in message.content:
+            parts = message.content.split(" ", 1)  # Split at the first space
+            if len(parts) > 1:
+                number = parts[1]
+            try:
+                int(number)
+                response = requests.get(f"https://www.thebluealliance.com/api/v3/team/frc{number}",
+                headers={
+                "X-TBA-Auth-Key": auth_key}
+                )
+                json_parsed = response.json()
+                embed = discord.Embed(title = 'Team ' + number, color = discord.Colour(0x0000FF),)
+                embed.add_field(name="🏫 School Name", value=json_parsed["school_name"], inline=False)
+                embed.add_field(name="📍 Location", value=f"{json_parsed['city']}, {json_parsed['state_prov']}, {json_parsed['country']}", inline=False)
+                await message.channel.send(embed=embed)
+            except ValueError:
+                await message.channel.send("Invalid Team Number")
+                
+            
 
-
-
-bot.run(Token)
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(Token)
