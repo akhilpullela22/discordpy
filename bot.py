7c80b6fd6bdb14150ee311753043a7c0cb8a18a5
Akhil Pullela <44563625+akhilpullela22@users.noreply.github.com>
2025-06-13 15:22:55 -0400

    awards needs some work

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import asyncio
 #from googletrans import Translator
 import requests
 from discord.ext import commands
+from collections import defaultdict
+
 
 with open("C:\\Users\\akhil\\Documents\\Bot_keys\\bot_token.txt", "r") as file:
     Token = file.read()
@@ -21,9 +23,11 @@ class MyClient(discord.Client):
         # don't respond to ourselves
         if message.author == self.user:
             return
-
+        
+        if '!help' in message.content:
+            await message.channel.send("!team + number for general information regarding a team \n !awards + number for award history of a team \n !events + team + year for events data for teams during a specific year")
         if '!team' in message.content:
-            parts = message.content.split(" ", 1)  # Split at the first space
+            parts = message.content.split(" ", 1)
             if len(parts) > 1:
                 number = parts[1]
             try:
@@ -33,15 +37,38 @@ class MyClient(discord.Client):
                 "X-TBA-Auth-Key": auth_key}
                 )
                 json_parsed = response.json()
-                embed = discord.Embed(title = 'Team ' + number, color = discord.Colour(0x0000FF),)
-                embed.add_field(name="🏫 School Name", value=json_parsed["school_name"], inline=False)
-                embed.add_field(name="📍 Location", value=f"{json_parsed['city']}, {json_parsed['state_prov']}, {json_parsed['country']}", inline=False)
-                embed.add_field(name="🏅 Established", value=str(json_parsed["rookie_year"]), inline=False)
+                embed = discord.Embed(title = 'Team ' + number, color = discord.Colour(0x0000FF),description=json_parsed["nickname"])
+                embed.add_field(name="🏫 **School**", value=json_parsed["school_name"], inline=False)
+                embed.add_field(name="📍 **Location**", value=f"{json_parsed['city']}, {json_parsed['state_prov']}, {json_parsed['country']}", inline=False)
+                embed.add_field(name="🎯 **Established**", value=str(json_parsed["rookie_year"]), inline=False)
+                embed.set_footer(text="Data provided by The Blue Alliance")
+                embed.timestamp = discord.utils.utcnow()
                 await message.channel.send(embed=embed)
             except ValueError:
                 await message.channel.send("Invalid Team Number")
-                
-            
+        if '!awards' in message.content:
+            parts = message.content.split(" ", 1)
+            if len(parts) > 1:
+                number = parts[1]
+            response = requests.get(f"https://www.thebluealliance.com/api/v3/team/frc{number}/history",
+                headers={
+                "X-TBA-Auth-Key": auth_key}
+                )
+            json_parsed = response.json()
+            awards_by_name = defaultdict(list)
+            for award in json_parsed["awards"]:
+                awards_by_name[award["name"]].append(str(award["year"]))
+
+
+            for award_name, years in awards_by_name.items():
+                embed = discord.Embed(
+                    title=f'Award: {award_name}', 
+                    color=discord.Colour(0x0000FF), 
+                    description=f'Years: {", ".join(years)}'
+                )
+                embed.set_footer(text="Data provided by The Blue Alliance")
+                embed.timestamp = discord.utils.utcnow()
+                await message.channel.send(embed=embed)
 
 intents = discord.Intents.default()
 intents.message_content = True
